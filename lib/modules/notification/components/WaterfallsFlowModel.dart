f316cbe6efd5d00278b6f1652c610c88d247a96e
af-liu <18264480900@163.com>
2023-05-08 22:57:09 +0800

    feat: ...

--- lib/modules/notification/components/WaterfallsFlowModel.dart
+++ lib/modules/notification/components/WaterfallsFlowModel.dart
@@ -1,20 +1,106 @@
-class WaterfallsFlowModel {
-  String? imageUrl;
-  String? content;
-  String? name;
-  String? avatarUrl;
-  String? like;
-  bool? isLise;
-  int? imageWidth;
-  int? imageHeight;
+import 'dart:async';
 
-  WaterfallsFlowModel(
-      {this.imageUrl,
-      this.content,
-      this.name,
-      this.avatarUrl,
-      this.like,
-      this.isLise,
-      this.imageWidth,
-      this.imageHeight});
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter/material.dart';
+
+class WaterfallsFlowModel extends StatefulWidget {
+  const WaterfallsFlowModel({Key? key}) : super(key: key);
+
+  @override
+  State<WaterfallsFlowModel> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<WaterfallsFlowModel> {
+  final List<String> _imageUrls = [
+    'https://picsum.photos/id/10/400/400',
+    'https://picsum.photos/id/20/400/600',
+    'https://picsum.photos/id/30/400/500',
+    'https://picsum.photos/id/40/400/300',
+    'https://picsum.photos/id/50/400/700',
+    'https://picsum.photos/id/60/400/400',
+    'https://picsum.photos/id/70/400/800',
+    'https://picsum.photos/id/80/400/200',
+    'https://picsum.photos/id/90/400/900',
+    'https://picsum.photos/id/100/400/500',
+  ];
+
+  final List<double> _imageHeights = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 计算所有图片的高度
+    for (String url in _imageUrls) {
+      _calculateImageHeight(url);
+    }
+  }
+
+  Future<void> _calculateImageHeight(String url) async {
+    final completer = Completer<void>();
+
+    // 加载图片
+    final imageStream = NetworkImage(url).resolve(ImageConfiguration.empty);
+    final listener = ImageStreamListener(
+      (ImageInfo info, bool _) {
+        final double height = info.image.height.toDouble();
+        setState(() {
+          _imageHeights.add(height);
+        });
+        completer.complete();
+      },
+      onError: (exception, stackTrace) {
+        completer.completeError(exception);
+      },
+    );
+    // 监听图片加载完成事件
+    imageStream.addListener(listener);
+
+    // 等待图片加载完成
+    await completer.future;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('notification'),
+      ),
+      body: StaggeredGridView.extentBuilder(
+        itemCount: _imageUrls.length,
+        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+        maxCrossAxisExtent: 230,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        itemBuilder: (BuildContext context, int index) {
+          return AspectRatio(
+            aspectRatio: 1,
+            child: Image.network(
+              _imageUrls[index],
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        staggeredTileBuilder: (int index) {
+          // 计算图片在瀑布流中的高度
+          final double imageHeight =
+              _imageHeights.isNotEmpty && _imageHeights.length > index
+                  ? _imageHeights[index]
+                  : 100;
+          late double imageHeightMiddleware = imageHeight;
+          if (imageHeight < 100) {
+            imageHeightMiddleware = 100;
+          }
+          if (imageHeight > 300) {
+            imageHeightMiddleware = 250;
+          }
+          const double textHeight = 30; // 假设每个图片下面有30高度的文字
+          final int numLines = (textHeight / 20).ceil(); // 假设每行文字高度为20
+          final double totalHeight =
+              imageHeightMiddleware + numLines * textHeight;
+          return StaggeredTile.extent(1, totalHeight);
+        },
+      ),
+    );
+  }
 }
--- lib/modules/notification/notification.dart
+++ lib/modules/notification/notification.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'components/WaterfallsFlowModel.dart';
 
 class NotificationContainer extends StatefulWidget {
   const NotificationContainer({Key? key}) : super(key: key);
@@ -11,44 +12,6 @@ class NotificationContainer extends StatefulWidget {
 class _NotificationContainerState extends State<NotificationContainer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notification-'),
-      ),
-      key: UniqueKey(),
-      body: StaggeredGridView.countBuilder(
-        crossAxisCount: 4,
-        itemCount: 30,
-        itemBuilder: (BuildContext context, int index) => _buildCard(index),
-        staggeredTileBuilder: (int index) =>
-            StaggeredTile.count(2, index.isEven ? 2.5 : 2.6),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-      ),
-    );
-  }
-
-  Widget _buildCard(int index) {
-    return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AspectRatio(
-            aspectRatio: 1.0,
-            child: Image.network(
-              'https://picsum.photos/id/${index + 10}/500/500',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'This is card $index',
-              style: const TextStyle(fontSize: 16.0),
-            ),
-          ),
-        ],
-      ),
-    );
+    return const WaterfallsFlowModel();
   }
 }
